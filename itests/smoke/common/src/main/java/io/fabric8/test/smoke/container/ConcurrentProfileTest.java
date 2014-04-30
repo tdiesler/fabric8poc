e527638c813a481db0a659bcecc76c955f681389
Thomas Diesler <thomas.diesler@jboss.com>
2014-04-30 12:23:57 +0200

    Skip ConcurrentProfileTest for Karaf

--- itests/smoke/common/src/main/java/io/fabric8/test/smoke/container/ConcurrentProfileTest.java
+++ itests/smoke/common/src/main/java/io/fabric8/test/smoke/container/ConcurrentProfileTest.java
@@ -38,6 +38,7 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.test.gravia.itests.support.AnnotatedContextListener;
 import org.jboss.test.gravia.itests.support.ArchiveBuilder;
+import org.junit.Assume;
 import org.junit.runner.RunWith;
 
 /**
@@ -75,5 +76,11 @@ public class ConcurrentProfileTest extends ConcurrentProfileTestBase {
             }
         });
         return archive.getArchive();
+    }
+
+    @Override
+    public void testConcurrentProfiles() throws Exception {
+        Assume.assumeFalse(RuntimeType.getRuntimeType() == RuntimeType.KARAF);
+        super.testConcurrentProfiles();
     }
 }
